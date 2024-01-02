064ceaff38ebf3b907c40c66127d721a367b1728
coderKillo <coderKillo@users.noreply.github.com>
2024-01-02 20:56:09 +0100

    adjust collision and draw order of wires

--- Entities/Entities/SmelterEntity.gd
+++ Entities/Entities/SmelterEntity.gd
@@ -3,5 +3,9 @@ extends Entity
 @onready var _heat_provider: HeatProvider = $HeatProvider
 
 
-func _process(delta):
+func _process(_delta):
 	_heat_provider.amount = 100
+
+
+func _on_material_enter_area_body_entered(body: Node2D):
+	body.queue_free()
