ea8696576e0a62d952c4eeb52e7d899f20011db7
coderKillo <coderKillo@users.noreply.github.com>
2024-01-15 17:22:45 +0100

    add fire to smelter

--- Entities/Entities/SmelterEntity.gd
+++ Entities/Entities/SmelterEntity.gd
@@ -1,6 +1,11 @@
 extends Entity
 
 @onready var _heat_provider: HeatProvider = $HeatProvider
+@onready var _fire: AnimatedSprite2D = $FireAnimation
+
+
+func _ready():
+	Events.system_tick.connect(_on_system_tick)
 
 
 func _process(_delta):
@@ -13,3 +18,10 @@ func _on_material_enter_area_body_entered(body: Node2D):
 
 	if body.is_in_group("ore"):
 		body.queue_free()
+
+
+func _on_system_tick(_delta):
+	if _heat_provider.amount > 0:
+		_fire.show()
+	else:
+		_fire.hide()
