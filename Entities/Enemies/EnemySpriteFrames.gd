5fa99ea7453bcf08ac972515c3e22c512298d578
coderKillo <coderKillo@users.noreply.github.com>
2024-02-06 11:01:08 +0100

    add parts

--- Entities/Enemies/EnemySpriteFrames.gd
+++ Entities/Enemies/EnemySpriteFrames.gd
@@ -8,16 +8,17 @@ const DEATH = "death"
 
 
 func _init():
-	add_animation(WALK)
-	set_animation_loop(WALK, false)
-	set_animation_speed(WALK, 5)
+	if Engine.is_editor_hint():
+		add_animation(WALK)
+		set_animation_loop(WALK, false)
+		set_animation_speed(WALK, 5)
 
-	add_animation(ATTACK)
-	set_animation_loop(ATTACK, true)
-	set_animation_speed(ATTACK, 5)
+		add_animation(ATTACK)
+		set_animation_loop(ATTACK, true)
+		set_animation_speed(ATTACK, 5)
 
-	add_animation(DEATH)
-	set_animation_loop(DEATH, false)
-	set_animation_speed(DEATH, 5)
+		add_animation(DEATH)
+		set_animation_loop(DEATH, false)
+		set_animation_speed(DEATH, 5)
 
-	remove_animation("default")
+		remove_animation("default")
