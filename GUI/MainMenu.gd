bddf475e7cb209f729320fea2756bf598680a208
coderKillo <coderKillo@users.noreply.github.com>
2024-03-03 08:52:31 +0100

    add main menu

---
+++ GUI/MainMenu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var _start_button: Button = $VBoxContainer/StartButton
+@onready var _quit_button: Button = $VBoxContainer/QuitButton
+
+
+func _ready():
+	_start_button.pressed.connect(_on_start_pressed)
+	_quit_button.pressed.connect(_on_quit_pressed)
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Simulation.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
