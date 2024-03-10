91faec14460de0910efbed46238ffe6a42512941
coderKillo <coderKillo@users.noreply.github.com>
2024-03-10 19:57:22 +0100

    limit body_drag count

--- Entities/Player/DragObjects.gd
+++ Entities/Player/DragObjects.gd
@@ -2,8 +2,8 @@ class_name DragObjects
 extends Area2D
 
 @export var max_distance = 9
+@export var max_bodies = 1
 
-var _bodies = []
 var _grab = false
 
 
@@ -16,22 +16,16 @@ func release():
 
 
 func bodies_grabed() -> int:
-	return len(_bodies)
+	return len(get_bodies())
 
 
 func _draw():
-	if not _grab:
-		return
-
-	for body in get_overlapping_bodies():
+	for body in get_bodies():
 		draw_line(global_position, body.global_position, Color.BLACK, 1.0)
 
 
 func _physics_process(_delta):
-	if not _grab:
-		return
-
-	for body in get_overlapping_bodies():
+	for body in get_bodies():
 		var distance = body.global_position.distance_to(global_position)
 		var direction = body.global_position.direction_to(global_position)
 		var entity := body as GroundEntity
@@ -43,3 +37,16 @@ func _physics_process(_delta):
 			continue
 
 		body.global_position += direction * (distance - max_distance)
+
+
+func get_bodies() -> Array:
+	var bodies = []
+	if not _grab:
+		return bodies
+
+	for body in get_overlapping_bodies():
+		bodies.append(body)
+		if len(bodies) >= max_bodies:
+			break
+
+	return bodies
