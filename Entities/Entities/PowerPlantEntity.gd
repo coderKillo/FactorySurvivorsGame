243799da2f7425da6db957fc60a16e16836b057f
coderKillo <coderKillo@users.noreply.github.com>
2024-09-10 21:58:52 +0200

    add: lighning visuals when power plant is running

--- Entities/Entities/PowerPlantEntity.gd
+++ Entities/Entities/PowerPlantEntity.gd
@@ -4,6 +4,7 @@ extends Entity
 @onready var _power_source: PowerSource = $PowerSource
 @onready var _heat_receiver: HeatReceiver = $HeatReceiver
 @onready var _molt_bucket: Bucket = $MoltBucket
+@onready var _lightning: AnimatedSprite2D = $LightningAnimation
 
 
 func _ready():
@@ -26,6 +27,21 @@ func _on_system_tick(_delta):
 
 	if _power_source.efficency > 0.0:
 		_animation.play("active")
+		_show_lightning()
 		SoundManager.play("powerplant_active")
 	if _power_source.efficency == 0.0:
 		_animation.play("idle")
+
+
+func _show_lightning():
+	if _lightning.is_playing():
+		return
+
+	var random_value := randi() % 100
+
+	if random_value < 5:
+		_lightning.play("Lightning1")
+	elif random_value < 30:
+		_lightning.play("Lightning2")
+	elif random_value < 50:
+		_lightning.play("Lightning3")
